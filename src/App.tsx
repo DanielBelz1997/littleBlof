56f32ca14d3abc43a851392b55b7c19fef65ab2d
BelzDaniel <BelzDaniel6@gmail.com>
2024-08-16 00:47:07 +0300

    first init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Mission from "./Mission";
+
+function App() {
+  return <>hello!</>;
+}
+
+export default App;
